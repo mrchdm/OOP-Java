02350b4200020dec3ce6a7f43648b98d6d546678
mrchdm <dementevamm@yandex.ru>
2023-02-23 20:17:14 +0300

    operation

---
+++ Model/enums/OperationType.java
@@ -0,0 +1,15 @@
+package Model.enums;
+
+/**
+ * Mathematical operations
+ */
+public enum OperationType {
+    DIVIDE(),
+    MULTIPLY(),
+    SUMMARIZE(),
+    DIFFERENCE();
+
+    OperationType() {
+    }
+
+}
