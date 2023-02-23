520bfa6376d0bd8ea2f1c8d5e157ffcc70380b5d
mrchdm <dementevamm@yandex.ru>
2023-02-23 20:17:59 +0300

    example2

---
+++ Model/RationalExample.java
@@ -0,0 +1,13 @@
+package Model;
+
+import Model.enums.OperationType;
+
+/**
+ * Class for working with rational numbers
+ */
+public class RationalExample extends Example<Number> {
+
+    public RationalExample(OperationType operation) {
+        super(operation);
+    }
+}
