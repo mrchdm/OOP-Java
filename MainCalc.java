ef640957b15f4b327abe91390a8b0db84907d2db
mrchdm <dementevamm@yandex.ru>
2023-02-23 20:12:52 +0300

    create main block

---
+++ MainCalc.java
@@ -0,0 +1,8 @@
+import Controller.Calculate;
+
+public class MainCalc {
+    public static void main(String[] args) {
+        Calculate calculate = new Calculate();
+        calculate.start();
+    }
+}
